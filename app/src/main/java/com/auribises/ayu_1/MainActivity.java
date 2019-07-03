25e579115463a5e34ee982f21d4fd3540d2d6000
AYUSHI SHARMA <ayushisharma5141@gmail.com>
2019-07-03 09:39:49 +0530

    Android Intro

---
+++ app/src/main/java/com/auribises/ayu_1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.auribises.ayu_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Linking the layout of activity
+
+    }
+}
